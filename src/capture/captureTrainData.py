715290ec8441da17dd73e3b214d39fecf52c67ee
Nick Phillips <np1970@my.bristol.ac.uk>
2014-11-24 22:19:57 +0000

    added more gesture trials and tidied up a bit

--- src/capture/captureTrainData.py
+++ src/capture/captureTrainData.py
@@ -6,7 +6,6 @@ sys.path.append(os.path.abspath("../"))
 from utils.streamUtils.StreamRead import StreamRead
 
 class main():
-
 	def __init__(self, inputArgs):
 		pygame.init()
 	 	size = width, height = 800, 800
@@ -26,7 +25,16 @@ class main():
 	 	#open stream reader
 		self.stream = StreamRead(inputArgs.streamFile)
 
-		self.gesture_classes = {"REST" : self.captureREST, "LEFT-FOOT-STAMP" : self.captureLEFT_FOOT_STAMP, "LEFT-FRONT-UP" : self.captureLEFT_FRONT_UP}
+		self.gesture_classes = {"REST" : self.captureREST,
+		 						"LEFT-FOOT-STOMP" : self.captureLEFT_FOOT_STOMP,
+								"LEFT-TOE-UP" : self.captureLEFT_TOE_UP,
+								"LEFT-TOE-TAP" : self.captureLEFT_TOE_TAP,
+								"LEFT-HEEL-TAP" : self.catureLEFT_HEEL_TAP,
+								"LEFT-HEEL-RAISE" : self.captureLEFT_HEEL_RAISE,
+								"LEFT-FOOT-FLICKLEFT" : self.captureLEFT_FOOT_FLICKLEFT,
+								"LEFT-FOOT-FLICKRIGHT" : self.captureLEFT_FOOT_FLICKRIGHT,
+								"LEFT-FOOT-SPINCW" : self.captureLEFT_FOOT_SPINCW,
+								"LEFT-FOOT-SPINACW" : self.captureLEFT_FOOT_SPINACW}
 		#generate trial orders
 		trials = self.genTrials(inputArgs.num_trials)
 
@@ -63,7 +71,7 @@ class main():
 
 	#Display message and wait for next test
 	def endTrial(self):
-		self.textToScreen("Thankyou, please rest your feet and wait for the next test")
+		self.textToScreen("Thank you. Please rest your feet and wait for the next test.")
 		time.sleep(2)
 
 	## File Utilities ##
@@ -78,24 +86,23 @@ class main():
 		file.write(enc_data)
 
 	## Trial Methods ##
-
-	#Capture resting gesture
-	def captureREST(self, dir_name):
-		self.textToScreen("Please press SPACE and then keep your feet at rest for the next 5 seconds")
+	# Base capture method - Press space, do gesture and hold for specified time
+	def captureHold(self, dir_name, gesture, displayText, holdLength):
+		self.textToScreen(displayText)
 		ready = False
 
-		#wait for user to being test
+		# Wait for user to begin test
 		while not ready:
 			for event in pygame.event.get():
 				if event.type == pygame.KEYDOWN :
 					if event.key == pygame.K_SPACE :
 						ready = True
 
-		self.textToScreen("Please wait 5 seconds")
+		self.textToScreen("Please wait " + str(holdLength) + " seconds")
 
-		#Capture for 1 second 5 times
-		for i in xrange(0, 5):
-			output_file = self.makeFile(dir_name, "REST")
+		# Capture data for as long as holdLength (seconds)
+		for i in xrange(0, holdLength):
+			output_file = self.makeFile(dir_name, gesture)
 			self.stream.emptyStreamBuffer()
 			stop = time.time() + 1
 			while time.time() < stop:
@@ -103,17 +110,16 @@ class main():
 				for record in records:
 					self.writeData(output_file, record)
 
-		
-	# Capture left foot stamp
-	def captureLEFT_FOOT_STAMP(self, dir_name):
-		self.textToScreen("Please tap SPACE, Stamp your left foot and then tap SPACE as soon as you are done")
+	# Base capture method - Press space, do gesture, then press space
+	def captureExplicit(self, dir_name, gesture, displayText):
+		self.textToScreen(displayText)
 
-		output_file = self.makeFile(dir_name, "LEFT-FOOT-STAMP")
+		output_file = self.makeFile(dir_name, gesture)
 
 		ready = False
 		done = False
 
-		#Wait for user to begin test
+		# Wait for user to begin test
 		while not ready:
 			for event in pygame.event.get():
 				if event.type == pygame.KEYDOWN :
@@ -121,7 +127,7 @@ class main():
 						ready = True
 
 		self.stream.emptyStreamBuffer()
-		#read data until space is pressed
+		# Capture data until space is pressed
 		while not done:
 			records = self.stream.readFromStream()
 			for record in records:
@@ -132,33 +138,48 @@ class main():
 					if event.key == pygame.K_SPACE :
 						done = True
 
-	#Capture toe lifted
-	def captureLEFT_FRONT_UP(self, dir_name):
-		self.textToScreen("Please elevate the front of your left foot,")
-		self.textToScreen("press SPACE and maintain the position for 5 seconds", y_pos=40)
-		ready = False
+	# Capture resting gesture for 5 seconds
+	def captureREST(self, dir_name):
+		displayText = "Press SPACE and then keep your feet at rest for the next 5 seconds"
+		self.captureHold(dir_name, "REST", displayText, 5)
 
-		#wait for user to being test
-		while not ready:
-			for event in pygame.event.get():
-				if event.type == pygame.KEYDOWN :
-					if event.key == pygame.K_SPACE :
-						ready = True
+	# Capture left foot stomp gesture
+	def captureLEFT_FOOT_STOMP(self, dir_name):
+		displayText = "Press SPACE, Stamp your left foot and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-FOOT-STOMP", displayText)
 
-		self.textToScreen("Please wait 5 seconds")
+	# Capture toe lifted
+	def captureLEFT_TOE_UP(self, dir_name):
+		displayText = "Press space, raise your left toe while your heel is on the ground, and hold for 5 seconds"
+		self.captureHold(dir_name, "LEFT-TOE-UP", displayText, 5)
 
-		#Capture for 1 second 5 times
-		for i in xrange(0, 5):
-			output_file = self.makeFile(dir_name, "LEFT-FRONT-UP")
-			self.stream.emptyStreamBuffer()
-			stop = time.time() + 1
-			while time.time() < stop:
-				records = self.stream.readFromStream()
-				for record in records:
-					self.writeData(output_file, record)
+	def captureLEFT_TOE_TAP(self, dir_name):
+		displayText = "Press SPACE, tap your left toe once and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-TOE-TAP", displayText)
 
+	def captureLEFT_HEEL_TAP(self, dir_name):
+		displayText = "Press SPACE, tap your left heel once and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-HEEL-TAP", displayText)
 
+	def captureLEFT_HEEL_RAISE(self, dir_name):
+		displayText = "Press SPACE, raise your left heel, and hold for five seconds"
+		self.captureHold(dir_name, "LEFT-HEEL-RAISE", displayText, 5)
 
+	def captureLEFT_FOOT_FLICKLEFT(self, dir_name):
+		displayText = "Press SPACE, flick your left foot to the left, and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-FOOT-FLICKLEFT", displayText)
+
+	def captureLEFT_FOOT_FLICKRIGHT(self, dir_name):
+		displayText = "Press SPACE, flick your left foot to the right, and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-FOOT-FLICKRIGHT", displayText)
+
+	def captureLEFT_FOOT_SPINCW(self, dir_name):
+		displayText = "Press SPACE, spin your left foot clockwise, and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-FOOT-SPINCW", displayText)
+
+	def captureLEFT_FOOT_SPINACW(self, dir_name):
+		displayText = "Press SPACE, spin your left foot anticlockwise, and then press SPACE as soon as you are done"
+		self.captureExplicit(dir_name, "LEFT-FOOT-SPINACW", displayText)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "Capture traing data for smartshoes")
@@ -167,6 +188,3 @@ if __name__ == "__main__":
 	parser.add_argument("-s", action = "store", default = "/dev/input/smartshoes", type = str, dest = "streamFile", help = "The Name of the stream to read input from (default: /dev/input/smartshoes)")
 	inputArgs = parser.parse_args()
 	main(inputArgs)
-
-	
-	
