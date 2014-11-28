4ca2bc316f554bd6f607fd6396d58d1322718590
Nick Phillips <np1970@my.bristol.ac.uk>
2014-11-28 16:36:48 +0000

    added maya capture file debug mode

---
+++ src/maya/mayaCapture.py
@@ -0,0 +1,36 @@
+# Module for getting device input and talking to Maya to get model to update
+import socket, time, json, random, argparse
+
+def main(args):
+    maya = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect to Maya (Maya must be listening)
+    maya.connect(("127.0.0.1", 7777))
+
+    vals = {"tx" : 0, "ty" : 0, "tz" : 0, "rx" : 0, "ry" : 0, "rz" : 0}
+    data = {"ANKLE_L" : vals, "TOE_L" : vals, "ANKLE_R" : vals, "TOE_R" : vals}
+
+    startTime = time.time()
+    duration = 60
+
+    # DEBUGGING (RUN WITHOUT DEVICE)
+    if args.debug:
+        while time.time() < startTime + duration:
+            # Random rotation values
+            data["ANKLE_L"]["rx"] = random.uniform(0,20)
+            data["ANKLE_L"]["ry"] = random.uniform(0,20)
+            data["ANKLE_L"]["rz"] = random.uniform(0,20)
+            maya.send(json.dumps(data))
+            time.sleep(0.1)
+        return
+
+    while time.time() < startTime + duration:
+        maya.send(json.dumps(data))
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = "Capture device data and display on Maya")
+    parser.add_argument("-d", action = "store_true", dest = "debug", help = "Turn debug mode on (run without hardware device)")
+    args = parser.parse_args()
+    if args.debug:
+        print("DEBUG MODE ENABLED")
+    main(args)
